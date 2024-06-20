fffc73486d0ded74c46410e48d0e3ac5ca34db2a
dayeonkimm <ekdyd516@gmail.com>
2024-06-20 15:01:51 +0900

    lint test

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,22 +1,21 @@
 import time
 from django.core.management.base import BaseCommand
-from django.db import connections # db와 연결을 시도
-
+from django.db import connections  # db와 연결을 시도
 from django.db.utils import OperationalError
 from psycopg2 import OperationalError as Psycopg2OperationalError
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        self.stdout.write('Waiting fof DB Connection ...')
-        
+        self.stdout.write('Waiting for DB Connection ...')
+
         is_db_connected = None
-        
+
         while not is_db_connected:
             try:
                 is_db_connected = connections['default']
-            except(OperationalError, Psycopg2OperationalError):
+            except (OperationalError, Psycopg2OperationalError):
                 self.stdout.write("Retry DB Connection ...")
                 time.sleep(1)
                 
-        self.stdout.write(self.style.SUCCESS('Success to PostgreSQL connection'))
+                
         
